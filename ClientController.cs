7e36052c72ced8482469baffee3d43dfca539d3e
GitHub-Neves <80914365+GitHub-Neves@users.noreply.github.com>
2021-05-18 21:20:09 +0100

    Controller - Client

---
+++ ClientController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NS_Fitness_Website.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Treinos()
+        {
+            return View();
+        }
+        public ActionResult PlanosNutricao()
+        {
+            return View();
+        }
+        public ActionResult Progresso()
+        {
+            return View();
+        }
+        public ActionResult Mensagens()
+        {
+            return View();
+        }
+    }
+}
