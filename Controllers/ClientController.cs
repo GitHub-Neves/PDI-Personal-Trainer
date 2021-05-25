fc17cdef27153e1dbb45178305d5853cd7fb6cac
GitHub-Neves <80914365+GitHub-Neves@users.noreply.github.com>
2021-05-25 22:53:09 +0100

    Add files via upload

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NS_Fitness_Website.Models;
+
+namespace NS_Fitness_Website.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        DatabasePDI db = new DatabasePDI();
+        public ActionResult Index(int? id)
+        {
+            perfil perfil = db.perfil.Find(id);
+            return View(perfil);
+
+        }
+        public ActionResult Treinos(int? id)
+        {
+            treinos treino = db.treinos.Find(id);
+            return View(treino);
+        }
+        public ActionResult PlanosNutricao(int? id)
+        {
+            user_clinica clinica = db.user_clinica.Find(id);
+            user_treino treino = db.user_treino.Find(id);
+            user_alimentacao alimentacao = db.user_alimentacao.Find(id);
+            user_plano plano = db.user_plano.Find(id);
+            return View();
+        }
+        public ActionResult Progresso()
+        {
+            return View();
+        }
+        public ActionResult Mensagens()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/TrainerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NS_Fitness_Website.Controllers
+{
+    public class TrainerController : Controller
+    {
+        // GET: Trainer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ClientePerfil()
+        {
+            return View();
+        }
+        public ActionResult ClienteTreinos()
+        {
+            return View();
+        }
+        public ActionResult ClienteNutricao()
+        {
+            return View();
+        }
+        public ActionResult ClienteProgresso()
+        {
+            return View();
+        }
+        public ActionResult ClienteMensagens()
+        {
+            return View();
+        }
+    }
+}
