8ed3e4636bf520e1db66bc1972c8ad149902c5c7
GitHub-Neves <80914365+GitHub-Neves@users.noreply.github.com>
2021-05-30 20:01:39 +0100

    Add files via upload

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -10,24 +10,24 @@ namespace NS_Fitness_Website.Controllers
     public class ClientController : Controller
     {
         // GET: Client
-        DatabasePDI db = new DatabasePDI();
+        DatabaseWebsite db = new DatabaseWebsite();
         public ActionResult Index(int? id)
         {
-            perfil perfil = db.perfil.Find(id);
+            perfil perfil = db.perfil.Find(id); 
             return View(perfil);
-
         }
         public ActionResult Treinos(int? id)
         {
-            treinos treino = db.treinos.Find(id);
-            return View(treino);
+            return View();
         }
         public ActionResult PlanosNutricao(int? id)
         {
-            user_clinica clinica = db.user_clinica.Find(id);
+            /*
+            user_clinica clinica = db.user_clinica.Select(clinica => clinica.userId = id);
             user_treino treino = db.user_treino.Find(id);
             user_alimentacao alimentacao = db.user_alimentacao.Find(id);
             user_plano plano = db.user_plano.Find(id);
+            */
             return View();
         }
         public ActionResult Progresso()
--- Controllers/TrainerController.cs
+++ Controllers/TrainerController.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NS_Fitness_Website.Models;
 
 namespace NS_Fitness_Website.Controllers
 {
     public class TrainerController : Controller
     {
         // GET: Trainer
+        DatabaseWebsite db = new DatabaseWebsite();
         public ActionResult Index()
         {
-            return View();
+            return View(db.perfil.ToList());
         }
-        public ActionResult ClientePerfil()
+        public ActionResult ClientePerfil(int? id)
         {
-            return View();
+            perfil perfil = db.perfil.Find(id);
+            return View(perfil);
         }
         public ActionResult ClienteTreinos()
         {
             return View();
         }
-        public ActionResult ClienteNutricao()
+        public ActionResult ClienteNutricao(int? id)
         {
-            return View();
+            IEnumerable<user_clinica> clinica = db.user_clinica.Where(x => x.perfil_id == id );          
+            return View(clinica);
         }
         public ActionResult ClienteProgresso()
         {
